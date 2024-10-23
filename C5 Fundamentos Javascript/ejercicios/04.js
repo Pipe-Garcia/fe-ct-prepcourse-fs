673ec55d2b8d5bb959caa0e567a4334fc5fe9353
Pipe-Garcia <pipeg1069@gmail.com>
2024-10-23 19:01:02 -0300

    Ejercicios fundamentos de JS

--- C5 Fundamentos Javascript/ejercicios/04.js
+++ C5 Fundamentos Javascript/ejercicios/04.js
@@ -1,6 +1,6 @@
 // Resuelve el siguiente problema matemático.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevaResta = 10 - null === 3;
+const nuevaResta = (10 - 3) === 7;
 
 module.exports = nuevaResta;
